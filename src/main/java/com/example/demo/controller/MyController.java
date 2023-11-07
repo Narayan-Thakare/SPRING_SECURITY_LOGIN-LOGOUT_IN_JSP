1503d1c0993df636f3c5cc9e43864cd7b79cbeda
Narayan Thakare <narayan111thakare@gmail.com>
2023-11-07 22:37:11 +0530

    first commit

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -2,14 +2,25 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class MyController {
-	
+
 	@RequestMapping("/")
-	String myfun()
+	public String home()
 	{
-		return "index.jsp";
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/login")
+	public String loginPage()
+	{
+		return "login.jsp";
+	}
+	
+	@RequestMapping("/logout-success")
+	public String logoutPage()
+	{
+		return "logout.jsp";
 	}
 	
 }
